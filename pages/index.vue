f20641dd7f11e79d497f276f28a4e378e4426b6c
hilarious23 <s.positex11@gmail.com>
2018-08-15 08:25:50 +0900

    creating nesting routes

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <section class="container">
     <nuxt-link to="/users">Users</nuxt-link>
-  </section>
+   </section>
 </template>
 
 <script>
---
+++ pages/users.vue
@@ -0,0 +1,25 @@
+<!-- /users/*は全部これが表示される -->
+<template>
+    <div>
+        <input type="text" v-model="userId">
+        <button @click="onLoadUser">Load User</button>
+        <!-- ここにusersフォルダのindex.vueが入る→methodsで_idのindex.vueに変わる -->
+        <nuxt-child />
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+           userId: '' 
+        }
+    },
+    methods: {
+        onLoadUser() {
+            this.$router.push('/users/' + this.userId )
+        }
+    }
+}
+</script>
+
